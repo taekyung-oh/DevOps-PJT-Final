9aaa955b7c745bba247247392a8e76b1b1f57356
LYQook <anx2030@naver.com>
2023-06-26 10:37:54 +0900

    feat: output

--- terraform/security-Config/cloudwatch.tf
+++ terraform/security-Config/cloudwatch.tf
@@ -5,3 +5,8 @@ resource "aws_cloudwatch_log_group" "ssm_automation_log_group" {
   name              = "/aws/events/ssm/automation"
   retention_in_days = 14   # 로그의 expire 기간
 }
+
+# automation 로그 그룹 아웃풋
+output "ssm_automation_log_output" {
+        value = "aws_cloudwatch_log_group.ssm_automation_log_group.arn"
+}
--- terraform/security-Hub/CWlogs.tf
+++ terraform/security-Hub/CWlogs.tf
@@ -1,8 +1,14 @@
 # securityhub 로그 그룹 생성
 resource "aws_cloudwatch_log_group" "securityhub_logs" {
     name = "securityhub_logs"
-
 }
+
+# securityhub 로그 그룹 아웃풋
+output "securityhub_log_output" {
+        value = "aws_cloudwatch_log_group.securityhub_logs.arn"
+}
+
+
 
 # resource "aws_cloudwatch_log_subscription_filter" "securityhub_logs" {
 #     name            = "securityhub_logs_subscription_filter"
