efdbb1d4df6fb108931dc1be1869bf4312f2a70f
Park-ChanKyu <pak82666@mgmail.com>
2023-06-26 10:40:59 +0900

    Feat: Backend Bucket change

--- terraform/system/Provider.tf
+++ terraform/system/Provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "bighead-project-tfstate"
+    bucket  = "terraform-bighead-bucket"
     key     = "system/terraform.tfstate"
     region  = "ap-northeast-2"
     encrypt = true
